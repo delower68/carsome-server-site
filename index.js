7a8992086c0e78947417153bf7923352e1a7ca08
delower68 <hdelower68@gmail.com>
2022-11-24 02:19:24 +0800

    mongodb setup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -10,6 +11,44 @@ const port = process.env.PORT || 8000 ;
 // middle ware 
 app.use(cors());
 app.use(express.json());
+
+
+
+// mondodb inisital code 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tkreg8z.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+// main function
+async function run(){
+    try{
+        
+    }
+    finally{
+
+    }
+}
+run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
