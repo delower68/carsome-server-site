ce455487f8c537b5dd5683c067226d011d5671de
delower68 <hdelower68@gmail.com>
2022-11-25 23:52:49 +0800

    get the all user bookings done

--- index.js
+++ index.js
@@ -76,6 +76,15 @@ async function run(){
         })
 
 
+        // get all booking data from mongodb 
+        app.get('/bookings', async(req, res)=>{
+            const email = req.query.email ;
+            const query = {email: email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+
     }
     finally{
 
